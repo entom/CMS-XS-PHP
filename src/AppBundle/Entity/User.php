260e2013619576163edf81600ccc46687cd2a2a2
Tomasz Nicieja <tomasznicieja@MacBook-Pro.local>
2017-01-15 19:19:35 +0100

    Admin Backend, User Frontend modules - with routing and basic configurations

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomasznicieja
+ * Date: 15.01.2017
+ * Time: 18:33
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
